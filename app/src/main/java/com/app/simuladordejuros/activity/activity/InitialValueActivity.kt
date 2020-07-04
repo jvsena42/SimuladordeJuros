ac28dcf301100bf4beae267bdfde381628d40a52
jvsena42 <jvsena16@gmail.com>
2020-07-03 21:03:17 -0300

    Initial commit

---
+++ app/src/main/java/com/app/simuladordejuros/activity/activity/InitialValueActivity.kt
@@ -0,0 +1,13 @@
+package com.app.simuladordejuros.activity.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.simuladordejuros.R
+
+class InitialValueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial_value)
+    }
+}
