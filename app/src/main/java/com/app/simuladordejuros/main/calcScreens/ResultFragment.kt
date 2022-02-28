b2631ce0ec13903eacea1dc7835fe24fb7f8cbcd
jvsena42 <jvsena16@gmail.com>
2022-02-28 11:00:22 -0300

    feat: fragment_result.xml layout

---
+++ app/src/main/java/com/app/simuladordejuros/main/calcScreens/ResultFragment.kt
@@ -0,0 +1,28 @@
+package com.app.simuladordejuros.main.calcScreens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.simuladordejuros.R
+import com.app.simuladordejuros.databinding.FragmentResultBinding
+
+class ResultFragment : Fragment() {
+
+    private lateinit var binding: FragmentResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentResultBinding.bind(view)
+    }
+
+}
