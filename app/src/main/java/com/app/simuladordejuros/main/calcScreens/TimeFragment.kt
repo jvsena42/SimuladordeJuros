ab4e7ab08b5723d92d7ad625853a286bb8a59c45
jvsena42 <jvsena16@gmail.com>
2022-02-28 10:50:13 -0300

    feat: fragment_time.xml layout

---
+++ app/src/main/java/com/app/simuladordejuros/main/calcScreens/TimeFragment.kt
@@ -0,0 +1,28 @@
+package com.app.simuladordejuros.main.calcScreens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.simuladordejuros.R
+import com.app.simuladordejuros.databinding.FragmentTimeBinding
+
+class TimeFragment : Fragment() {
+
+    private lateinit var binding: FragmentTimeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentTimeBinding.bind(view)
+    }
+
+}
