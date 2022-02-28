bed4584fdb996145c73d74a93fda27e48b10d449
jvsena42 <jvsena16@gmail.com>
2022-02-28 13:28:26 -0300

    feat: implement ApplicationValueFragment.kt

--- app/src/main/java/com/app/simuladordejuros/main/MainViewModel.kt
+++ app/src/main/java/com/app/simuladordejuros/main/MainViewModel.kt
@@ -5,21 +5,32 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.app.simuladordejuros.R
+import com.app.simuladordejuros.util.SingleLiveEvent
 
 class MainViewModel : ViewModel() {
     private var applicationModel = ApplicationModel()
 
-    private val _toastMessage = MutableLiveData<Int>()
+    private val _toastMessage = SingleLiveEvent<Int>()
     val toastMessage: LiveData<Int> = _toastMessage
 
-    private val _nextScreen = MutableLiveData<Int>()
+    private val _nextScreen = SingleLiveEvent<Int>()
     val nextScreen: LiveData<Int> = _nextScreen
 
     fun setInitialValue(value: Editable?) {
         if (!value.isNullOrEmpty()) {
             val initialValue = value.toString().toDouble()
-            applicationModel.aplicationValue = initialValue
+            applicationModel.initialValue = initialValue
             _nextScreen.value = R.id.action_initialValueFragment_to_applicationValueFragment
+        } else {
+            errorMessage()
+        }
+    }
+
+    fun setApplicationValue(value: Editable?) {
+        if (!value.isNullOrEmpty()) {
+            val initialValue = value.toString().toDouble()
+            applicationModel.aplicationValue = initialValue
+            _nextScreen.value = R.id.action_applicationValueFragment_to_taxFragment
         } else {
             errorMessage()
         }
--- app/src/main/java/com/app/simuladordejuros/main/calcScreens/ApplicationValueFragment.kt
+++ app/src/main/java/com/app/simuladordejuros/main/calcScreens/ApplicationValueFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.app.simuladordejuros.R
 import com.app.simuladordejuros.databinding.FragmentApplicationValueBinding
+import com.app.simuladordejuros.main.MainViewModel
 
 class ApplicationValueFragment : Fragment() {
 
     private lateinit var binding: FragmentApplicationValueBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +25,13 @@ class ApplicationValueFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentApplicationValueBinding.bind(view)
+        onCLick()
+    }
+
+    private fun onCLick() = binding.run {
+        fabNext.setOnClickListener {
+            viewModel.setApplicationValue(applicationValueETX.text)
+        }
     }
 
 }
